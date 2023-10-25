3aacb365e5ee7a167b642a0f19573b49db9f5d55
hamskid-id <lawalhamzat27@gmail.com>
2023-10-25 00:39:55 -0700

    updated vitConfig

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,23 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv  } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const EnvPickedKeys = [
+  "NEXT_PUBLIC_SERVICE_ID",
+  "NEXT_PUBLIC_CONTACT_FORM_TEMPLATE_ID",
+  "NEXT_PUBLIC_PUBLIC_KEY"
+];
+
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  define: {
-    'process.env': {}
+export default defineConfig(({ mode }) => {
+
+  const env = loadEnv(mode, process.cwd(), '');
+  const processEnv = {};
+  EnvPickedKeys.forEach(key => processEnv[key] = env[key]);
+
+  return{
+    plugins: [react()],
+    define: {
+      'process.env':  processEnv
+    }
   }
 })
